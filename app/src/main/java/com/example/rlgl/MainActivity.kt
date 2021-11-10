8ef7d229f6427e8d6b8a01e21355b8a53d5d3ee3
RoyAppeldoorn <roy.appeldoorn@gmail.com>
2021-11-10 13:27:08 +0100

    Implemented seismic lib for shake detection

--- app/src/main/java/com/example/rlgl/MainActivity.kt
+++ app/src/main/java/com/example/rlgl/MainActivity.kt
@@ -14,11 +14,13 @@ import android.view.View
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.example.rlgl.databinding.ActivityMainBinding
+import com.squareup.seismic.ShakeDetector
 
-class MainActivity : AppCompatActivity(), SensorEventListener {
+class MainActivity : AppCompatActivity(), ShakeDetector.Listener {
 
-    private var running = false
     private var sensorManager: SensorManager? = null
+    private var shakeDetector: ShakeDetector? = null
+    private var steps = 0;
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +31,8 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         setContentView(view)
 
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        shakeDetector = ShakeDetector(this);
+        shakeDetector!!.start(sensorManager)
 
         if(ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED){
@@ -38,26 +42,8 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        running = true
-        val stepsSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
-
-        if (stepsSensor == null) {
-            Toast.makeText(this, "No Step Counter Sensor present on this phone!", Toast.LENGTH_SHORT).show()
-        } else {
-            sensorManager?.registerListener(this, stepsSensor, SensorManager.SENSOR_DELAY_UI)
-        }
-    }
-
-    override fun onSensorChanged(event: SensorEvent?) {
-        println(event!!.values[0])
-        if (running) {
-            val totalSteps = event!!.values[0];
-            binding.stepCounter.text = totalSteps.toString()
-        }
-    }
-
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+    override fun hearShake() {
+        steps++
+        binding.stepCounter.text = steps.toString()
     }
 }
