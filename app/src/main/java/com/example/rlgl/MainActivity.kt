288721e3cf4d1d5d999a3eccdf25683e33f9f0d8
RoyAppeldoorn <roy.appeldoorn@gmail.com>
2021-11-10 11:01:02 +0100

    Added pedometer

--- app/src/main/java/com/example/rlgl/MainActivity.kt
+++ app/src/main/java/com/example/rlgl/MainActivity.kt
@@ -1,11 +1,63 @@
 package com.example.rlgl
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.example.rlgl.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private var running = false
+    private var sensorManager: SensorManager? = null
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view: View = binding.root
+        setContentView(view)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(arrayOf(Manifest.permission.ACTIVITY_RECOGNITION), 1)
+            };
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        running = true
+        val stepsSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        if (stepsSensor == null) {
+            Toast.makeText(this, "No Step Counter Sensor present on this phone!", Toast.LENGTH_SHORT).show()
+        } else {
+            sensorManager?.registerListener(this, stepsSensor, SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        println(event!!.values[0])
+        if (running) {
+            val totalSteps = event!!.values[0];
+            binding.stepCounter.text = totalSteps.toString()
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
     }
 }
