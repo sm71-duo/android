556d8e9f2255bde6581e7bb592fb3e4f57cbe46f
Coen Breg <coenbreg@gmail.com>
2021-11-17 14:20:59 +0100

    feat: added new listener for acceleration

--- app/src/main/java/com/example/rlgl/viewmodels/MovementViewModel.kt
+++ app/src/main/java/com/example/rlgl/viewmodels/MovementViewModel.kt
@@ -3,5 +3,18 @@ package com.example.rlgl.viewmodels
 import androidx.lifecycle.ViewModel
 
 class MovementViewModel: ViewModel() {
+    var xMovement: Float = 0F
+    var yMovement: Float = 0F
+    var zMovement: Float = 0F
 
+
+    fun setMovement(x: Float, y: Float, z: Float){
+        xMovement = x
+        yMovement = y
+        zMovement = z
+    }
+
+    fun getMovementX(): Float {
+        return xMovement
+    }
 }
